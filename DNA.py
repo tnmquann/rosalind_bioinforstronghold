4a394bb8d4ef9188345eab80fc7315146bc3b954
tnmquann <minhquan.tdn.ct1619@gmail.com>
2023-03-02 16:25:46 +0700

    update sol1

--- DNA.py
+++ DNA.py
@@ -8,18 +8,18 @@ DNA_Nucleotides = ["A", "C", "G", "T"]
 # Define a function that validates a DNA sequence
 def validate_seq(seq):
   seq = seq.upper()
-  for nuc in seq:
-    if nuc not in DNA_Nucleotides:
+  for nucl in seq:
+    if nucl not in DNA_Nucleotides:
       return False
   return seq
 
 # Define a function that counts the frequency of each nucleotide in DNA sequence
-def count_nuc_freq(seq):
+def count_nucl_freq(seq):
   freq_dict = {"A":0, "C":0, "G":0, "T":0}
-  for nuc in seq:
-    freq_dict[nuc] += 1
+  for nucl in seq:
+    freq_dict[nucl] += 1
   return freq_dict
 
 seq = validate_seq(seq)
-freq_dict = count_nuc_freq(seq)
+freq_dict = count_nucl_freq(seq)
 print(freq_dict)
